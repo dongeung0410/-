9a743304e9d805c06d3037b7ddda4ecf3a98639b
dongeung0410 <163506979+dongeung0410@users.noreply.github.com>
2024-04-05 15:31:46 +0900

    Create 5주차.py
    5주차 학점

---
+++ 5주차.py
@@ -0,0 +1,10 @@
+q = input("점수를 입력하세요:")
+a = int(q)
+if a >= 71 :
+    print("A학점입니다")
+elif a <71 and a >=41 :
+        print("b학점입니다")
+elif a >= 11 and a < 41 :
+    print("c학점입니다")
+else :
+    print("d학점입니다")
