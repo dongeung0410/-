df131e5a87fe3fa08a09e30a6d1a9769c654319f
dongeung0410 <163506979+dongeung0410@users.noreply.github.com>
2024-04-19 15:43:08 +0900

    Update 7주차 파일.py
    수정본입니다

--- 7주차 파일.py
+++ 7주차 파일.py
@@ -2,6 +2,6 @@ numbers=[[1,2,3],[4,5,6],[7,8,9]]
 results=[ ]
 for k in numbers:
     for result in k:
-        if int(result) %2 == 0:
+        if result %2 == 0:
             results.append(result)
 print(results)
