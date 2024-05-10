1b8428baf2b162157be5927bc6d67ddcc19d4727
dongeung0410 <163506979+dongeung0410@users.noreply.github.com>
2024-05-10 16:48:38 +0900

    Update 10주차 퀴즈.py
    수정본 입니다.

--- 10주차 퀴즈.py
+++ 10주차 퀴즈.py
@@ -3,11 +3,8 @@ def lotto_numbers():
     results=[]
     while len(results)<6:
         random_numbers = random.randint(1,45)
-        if random_numbers in results:
-            print("results 안에 random_numbers가 있으므로, results 안에 추가하지 않습니다. ")
-        else:
+        if random_numbers not in results:
             results.append(random_numbers)
-            print(random_numbers)
     results.sort()
     return results
 print("생성된 로또 번호는",lotto_numbers(),"입니다")
