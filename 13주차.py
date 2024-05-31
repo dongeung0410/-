7ed874b2010d113469594074c2356b35afae2325
dongeung0410 <163506979+dongeung0410@users.noreply.github.com>
2024-05-31 15:47:14 +0900

    Update 13주차.py

--- 13주차.py
+++ 13주차.py
@@ -1,14 +1,13 @@
-
-
 class Drink:
     def __init__(self,name,n):
         self.menu={"커피":3000,"녹차":2500,"아이스티":3000}
         self.name = name
-        self.total = n
+        self.total = 0
+        self.count=n
 
     def sell(self):
         if self.name == "커피" or self.name == "녹차" or self.name == "아이스티":
-            print(self.name,n,self.menu[self.name]*self.total)
+            print(self.name,n,self.menu[self.name]*self.count)
         else:
             print("음료 주문이 틀렸습니다.")
 
